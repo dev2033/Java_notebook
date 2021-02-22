148186acf13641c2be100d0ea6440d6084513442
dev2033 <dev.elop.02@mail.ru>
2021-02-22 18:26:13 +0300

    add imageView

--- app/src/main/java/com/example/javanotebook/EditActivity.java
+++ app/src/main/java/com/example/javanotebook/EditActivity.java
@@ -1,20 +1,27 @@
 package com.example.javanotebook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.javanotebook.adapter.MainAdapter;
 import com.example.javanotebook.db.MyDbManager;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class EditActivity extends AppCompatActivity {
 
     private EditText edTitle, edDesc;
     private MyDbManager myDbManager;
+    private ImageView imNewImage;
+    private ConstraintLayout imageContainer;
+    private FloatingActionButton fbAddImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +33,9 @@ public class EditActivity extends AppCompatActivity {
     private void init() {
         edTitle = findViewById(R.id.edTitle);
         edDesc = findViewById(R.id.edDesc);
+        imageContainer = findViewById(R.id.imageContainer);
         myDbManager = new MyDbManager(this);
+        fbAddImage = findViewById(R.id.fbAddImage);
     }
 
     @Override
@@ -49,5 +58,18 @@ public class EditActivity extends AppCompatActivity {
             myDbManager.closeDb();
         }
     }
+
+    public void onClickDeleteImage(View view) {
+        /*Удаляет изображение + убирает imageContainer с экрана*/
+        imageContainer.setVisibility(View.GONE);
+        fbAddImage.setVisibility(View.VISIBLE);
+    }
+
+    public void onClickAddImage(View view) {
+        /*Добавляет изображение*/
+        imageContainer.setVisibility(View.VISIBLE);
+        view.setVisibility(View.GONE);
+    }
+
 
 }
